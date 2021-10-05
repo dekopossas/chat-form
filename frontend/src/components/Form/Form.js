3752935dc9c5f0f85cd57a115db59fbe3facc401
deko possas <andrepossaster@gmail.com>
2021-10-05 09:19:22 -0300

    add handles functions

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -3,22 +3,60 @@ import PropTypes from 'prop-types';
 
 import { Formik } from 'formik';
 
-const Form = ({ initialValues, onSubmit }) => (
-  <Formik initialValues={initialValues} onSubmit={onSubmit}>
-    {({ handleBlur, handleSubmit, handleChange }) => (
+const Form = ({ initialValues, handleSubmit }) => (
+  <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    {({ handleBlur, handleSubmit, handleChange, values }) => (
       <form className="form" onSubmit={handleSubmit}>
         <p>Olá, eu sou Chatnilson, Tudo bem? Para começarmos, preciso saber seu nome.</p>
-        <input className="form_fild" name="name" placeholder="Escreva seu nome" type="text" />
-        <p>
-          Que satisfação, "nome da pessoa". Agora que sei seu nome, qual a cidade que você mora?
-        </p>
-        <input className="form_fild" name="city" placeholder="Escolha sua cidade" type="text" />
-        <p>Qual o estado?</p>
-        <input className="form_fild" name="state" placeholder="Escolha seu estado" type="text" />
+        <input
+          className="form_fild"
+          name="name"
+          placeholder="Escreva seu nome"
+          type="text"
+          value={values.name}
+          onChange={handleChange}
+        />
+        <p>Que satisfação "nome da pessoa". Agora que sei seu nome, onde você mora?</p>
+        <input
+          className="form_fild"
+          name="state"
+          placeholder="Escolha seu estado"
+          type="text"
+          value={values.state}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+
+        <p>Qual o cidade?</p>
+        <input
+          className="form_fild"
+          name="city"
+          placeholder="Escolha sua cidade"
+          type="text"
+          value={values.city}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
         <p>Agora que sabem de onde esta falando, qual sua data de nascimento?</p>
-        <input className="form_fild" name="birthday" placeholder="Escolha uma data" type="text" />
+        <input
+          className="form_fild"
+          name="birthday"
+          placeholder="Escolha uma data"
+          type="text"
+          value={values.birthday}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
         <p>Por ultimo, o seu email:</p>
-        <input className="form_fild" name="email" placeholder="Escreva seu email" type="text" />
+        <input
+          className="form_fild"
+          name="email"
+          placeholder="Escreva seu email"
+          type="text"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
         <button className="form_btn" type="submit">
           Enviar
         </button>
@@ -30,7 +68,7 @@ const Form = ({ initialValues, onSubmit }) => (
 
 Form.propTypes = {
   initialize: PropTypes.object.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 };
 
 export default Form;
