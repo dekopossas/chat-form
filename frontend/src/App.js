01eda721f545d32cd0920a7039c5d01654cfd23c
deko possas <andrepossaster@gmail.com>
2021-10-04 21:36:30 -0300

    app formated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Form from './components/Form';
+
+const handleSubmit = (values) => alert(JSON.stringify(values));
+const initialValues = {};
 
 function App() {
   return (
     <div className="App">
       <h1>Form Chat</h1>
+      <Form handleSubmit={handleSubmit} initialValues={initialValues} />
     </div>
   );
 }
