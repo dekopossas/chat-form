56dc70f57dfcf7bed4ddaabfba1c7709a5ef2ee1
deko possas <andrepossaster@gmail.com>
2021-10-07 20:39:35 -0300

    error status done

--- frontend/src/pages/Suport/index.js
+++ frontend/src/pages/Suport/index.js
@@ -4,7 +4,14 @@ import api from '../../services/api';
 import Form from '../../components/Form';
 
 const initialValues = {};
-const handleSubmit = async (values) => await api.post('/suport', JSON.stringify(values));
+const handleSubmit = async (values) => {
+  const response = await api.post('/suport', JSON.stringify(values));
+  if (response.status === 200) {
+    alert('Contato salvo com sucesso.');
+  } else {
+    alert('Algo deu errado.');
+  }
+};
 
 function Suport() {
   return (
