4345fbd8b5899adf1aeb6306db2680d24c565783
deko possas <andrepossaster@gmail.com>
2021-10-07 22:09:41 -0300

    style exemple

--- frontend/src/pages/Suport/index.js
+++ frontend/src/pages/Suport/index.js
@@ -1,19 +1,37 @@
+// Package
 import React from 'react';
+import { useHistory } from 'react-router';
+
+// API
 import api from '../../services/api';
 
+// Components
 import Form from '../../components/Form';
 
-const initialValues = {};
-const handleSubmit = async (values) => {
-  const response = await api.post('/suport', values);
-  if (response.status === 201) {
-     return alert('Contato salvo com sucesso.');
-  } else {
-     return alert('Algo deu errado.');
-  }
-};
-
 function Suport() {
+  // ----------------------------------------------------------------
+  // States
+  // ----------------------------------------------------------------
+  const initialValues = {};
+
+  // ----------------------------------------------------------------
+  // Hooks
+  // ----------------------------------------------------------------
+  const history = useHistory();
+
+  // ----------------------------------------------------------------
+  // Functions
+  // ----------------------------------------------------------------
+  const handleSubmit = async (values) => {
+    const response = await api.post('/suport', values);
+    if (response.status === 201) {
+      alert('Contato salvo com sucesso.');
+      history.push('/');
+    } else {
+      alert('Algo deu errado.');
+    }
+  };
+
   return (
     <div>
       <h1>Suport Page</h1>
