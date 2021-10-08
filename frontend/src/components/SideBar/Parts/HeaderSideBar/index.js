4a0effd74ed65358dd380b3db294ae3734ed5073
deko possas <andrepossaster@gmail.com>
2021-10-07 22:53:31 -0300

    btn class name

--- frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -9,6 +9,9 @@ function HeaderSideBar() {
         src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png"
         alt="avatar"
       />
+      <div className={styles.buttons}>
+        ...
+      </div>
     </div>
   );
 }
