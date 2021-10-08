854cde6bc29dc517ddab5a19ba99856658b0f691
deko possas <andrepossaster@gmail.com>
2021-10-07 22:50:58 -0300

    new file of parts of sidebar

---
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderSideBar() {
+  return (
+    <div>
+      <img src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png" alt="avatar" />
+    </div>
+  )
+}
+
+export default HeaderSideBar
---
+++ frontend/src/components/SideBar/Parts/SearchSideBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../style.module.scss'
+
+function SearchSideBar() {
+  return (
+    <div className={styles.search}>
+      ...
+    </div>
+  )
+}
+
+export default SearchSideBar
