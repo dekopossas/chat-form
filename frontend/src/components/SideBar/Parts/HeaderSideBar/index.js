b966a6abdd3fb48a84ab491ee6774eea0a241a09
deko possas <andrepossaster@gmail.com>
2021-10-07 22:52:30 -0300

    style of image

--- frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from '../style.module.scss';
 
 function HeaderSideBar() {
   return (
     <div>
-      <img src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png" alt="avatar" />
+      <img
+        className={styles.header_avatar}
+        src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png"
+        alt="avatar"
+      />
     </div>
-  )
+  );
 }
 
-export default HeaderSideBar
+export default HeaderSideBar;
