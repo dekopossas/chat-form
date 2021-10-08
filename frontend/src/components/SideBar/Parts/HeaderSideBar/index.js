05cbef25352b6db5dae1e9ce4fee70bf58525077
deko possas <andrepossaster@gmail.com>
2021-10-07 23:13:03 -0300

    material ui

--- frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -1,4 +1,12 @@
+// Package
 import React from 'react';
+
+// Icons
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// Styles
 import style from './style.module.scss';
 
 function HeaderSideBar() {
@@ -10,7 +18,15 @@ function HeaderSideBar() {
         alt="avatar"
       />
       <div className={style.header_buttons}>
-        ...
+        <div className={style.header_icons}>
+          <DonutLargeIcon style={{color: '#919191'}} />
+        </div>
+        <div className={style.header_icons}>
+          <ChatIcon style={{color: '#919191'}} />
+        </div>
+        <div className={style.header_icons}>
+          <MoreVertIcon style={{color: '#919191'}} />
+        </div>
       </div>
     </div>
   );
