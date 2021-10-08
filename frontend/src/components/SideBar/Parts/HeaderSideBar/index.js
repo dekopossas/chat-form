154b74d85c168f4301cf309457ce693d740c9fad
deko possas <andrepossaster@gmail.com>
2021-10-07 23:00:31 -0300

    style change place

--- frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import styles from '../style.module.scss';
+import style from './style.module.scss';
 
 function HeaderSideBar() {
   return (
-    <div className={styles.header}>
+    <div className={style.header}>
       <img
-        className={styles.header_avatar}
+        className={style.header_avatar}
         src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png"
         alt="avatar"
       />
-      <div className={styles.buttons}>
+      <div className={style.buttons}>
         ...
       </div>
     </div>
--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeaderSideBar from './Parts/HeaderSideBar';
 import style from './style.module.scss';
 
 function SideBar() {
   return (
     <div className={style.sidebar}>
+      <HeaderSideBar />
       ...
     </div>
   )
