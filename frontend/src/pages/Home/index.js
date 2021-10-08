2e870321199dbaed4cec9a3c7affee9b9dffef9a
deko possas <andrepossaster@gmail.com>
2021-10-07 21:45:56 -0300

    new btn

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 function Home() {
+  const history = useHistory();
+
+  const goSuport = () => history.push('/suport');
+
   return (
     <div>
-      <h1>Page Home</h1>
+      <h1>Bem Vindo ao Atendimento Dinamico!</h1>
+      <Link>
+        <button onClick={goSuport}>Nova Observação</button>
+      </Link>
     </div>
   );
 }
