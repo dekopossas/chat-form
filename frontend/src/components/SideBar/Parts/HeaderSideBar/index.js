d340a3f871deaa3c8bff67b7fdeb2b8b7150d07c
deko possas <andrepossaster@gmail.com>
2021-10-07 22:55:22 -0300

    new class of header

--- frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/Parts/HeaderSideBar/index.js
@@ -3,7 +3,7 @@ import styles from '../style.module.scss';
 
 function HeaderSideBar() {
   return (
-    <div>
+    <div className={styles.header}>
       <img
         className={styles.header_avatar}
         src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png"
