b218b1bd615cd3fe08712a1b1c367be4477c33ba
deko possas <andrepossaster@gmail.com>
2021-10-15 19:35:24 -0300

    searchbar struturing

--- frontend/src/components/SideBar/PartsSideBar/SearchSideBar/index.js
+++ frontend/src/components/SideBar/PartsSideBar/SearchSideBar/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from '../style.module.scss'
+import React from 'react';
+import style from '../style.module.scss';
+
+// Icons (não consegui usar os indos decidi não perder tempo aqui mais!)
+// import SearchIcon from '@mui/icons-material/DonutLarge';
 
 function SearchSideBar() {
   return (
-    <div className={styles.search}>
-      ...
+    <div className={style.search}>
+      <div className={style.search_input}>
+        <div className={style.header_icons}>
+          {/* <SearchIcon fontSize="small" style={{color: '#919191'}} /> */}
+          <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchSideBar
+export default SearchSideBar;
