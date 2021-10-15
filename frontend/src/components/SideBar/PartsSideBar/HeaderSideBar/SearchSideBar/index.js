d55e3a957d72fafb5c05521d3331f3f3e82220d5
deko possas <andrepossaster@gmail.com>
2021-10-15 19:50:10 -0300

    render error, i need to think the solution

---
+++ frontend/src/components/SideBar/PartsSideBar/HeaderSideBar/SearchSideBar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './style.module.scss';
+
+// Icons (não consegui usar os indos decidi não perder tempo aqui mais!)
+// import SearchIcon from '@mui/icons-material/DonutLarge';
+
+function SearchSideBar() {
+  return (
+    <div className={style.search}>
+      <div className={style.search_input}>
+        <div className={style.search_icons}>
+          {/* <SearchIcon fontSize="small" style={{color: '#919191'}} /> */}
+          <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchSideBar;
