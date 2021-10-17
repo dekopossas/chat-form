81e5a1fa24fa5cb51915e88493ca84d2c92668c6
deko possas <andrepossaster@gmail.com>
2021-10-16 23:44:12 -0300

    remove input out of div icon

--- frontend/src/components/SideBar/PartsSideBar/SearchSideBar/index.js
+++ frontend/src/components/SideBar/PartsSideBar/SearchSideBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from '../style.module.scss';
+import style from './style.module.scss';
 
 // Icons (não consegui usar os indos decidi não perder tempo aqui mais!)
 // import SearchIcon from '@mui/icons-material/DonutLarge';
@@ -10,8 +10,8 @@ function SearchSideBar() {
       <div className={style.search_input}>
         <div className={style.search_icons}>
           {/* <SearchIcon fontSize="small" style={{color: '#919191'}} /> */}
-          <input type="search" placeholder="Procurar ou começar uma nova conversa" />
         </div>
+        <input type="search" placeholder="Procurar ou começar uma nova conversa" />
       </div>
     </div>
   );
--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import HeaderSideBar from './Parts/HeaderSideBar';
+import HeaderSideBar from './PartsSideBar/HeaderSideBar';
+import SearchSideBar from './PartsSideBar/SearchSideBar';
 import style from './style.module.scss';
 
 function SideBar() {
   return (
     <div className={style.sidebar}>
       <HeaderSideBar />
-      ...
+      <SearchSideBar />
     </div>
   )
 }
