12bef56bc4a9c37edb90f33453f48b4ab0b131eb
deko possas <andrepossaster@gmail.com>
2021-10-17 17:11:14 -0300

    render chat intro

---
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatIntro/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from 'style.module.scss'
+
+function ChatIntro() {
+  return (
+    <div className={style.chatIntro}>
+      <img src="" alt="" />
+      <h1>Mantenha seu Celular Conectado</h1>
+      <h2>O WhatsApp conecta ao seu celular para sincronizar suas mensagens. Para reduzir o uso de dados, conecte seu celular a uma rede Wi-Fi.</h2>
+    </div>
+  )
+}
+
+export default ChatIntro
--- frontend/src/components/MainContainer/index.js
+++ frontend/src/components/MainContainer/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ChatIntro from './PartsMainContainer/ChatIntro';
 import style from './style.module.scss';
 
 function MainContainer() {
   return (
     <div className={style.contentarea}>
-      ...
+      <ChatIntro />
     </div>
   )
 }
