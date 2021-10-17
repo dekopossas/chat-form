289aedb894219d4d620e47db568b279ca9bf0c11
deko possas <andrepossaster@gmail.com>
2021-10-17 19:16:08 -0300

    first version of reducer

---
+++ frontend/src/reducers/chatReducers.js
@@ -0,0 +1,18 @@
+// API
+import api from '../services/api';
+
+const allReports = api.get('/suport');
+
+const INITIAL_STATE = {
+  selectedName: {},
+  allReports: [...allReports],
+};
+
+const chatReducers = (state = INITIAL_STATE, action) => {
+  if (action.type === 'SELECT_REPORT') {
+    return {
+      ...state,
+      selectedName: action.name,
+    };
+  }
+};
