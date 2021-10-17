834ebfc3fb0d5e6b77d66934167597b7bfb64deb
deko possas <andrepossaster@gmail.com>
2021-10-17 00:13:45 -0300

    chat screen module

--- frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/ChatListComponents/ChatListItem.js
+++ frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/ChatListComponents/ChatListItem.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import style from './style.module.scss';
 
 function ChatListItem() {
   return (
-    <div>
-      ...
+    <div className={style.chatListItem}>
+      <img
+        className={style.chatList_avatar}
+        src="https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png"
+        alt="screen user chat"
+      />
+      <div className={style.chatList_lines}>
+        <div className={style.chatList_nickname}>André Possas</div>
+        <div className={style.chatList_date}>19:00</div>
+      </div>
+      <div className={style.chatList_lines}>
+        <div className={style.chatList_lastMsg}>
+          <p>Olá, Tudo bem?</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatListItem
+export default ChatListItem;
