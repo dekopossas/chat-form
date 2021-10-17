c68899e31ba0f9ea6dad8800d9cd785ea56a7517
deko possas <andrepossaster@gmail.com>
2021-10-17 19:00:16 -0300

    lint

--- frontend/src/components/MainContainer/index.js
+++ frontend/src/components/MainContainer/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
 import ChatIntro from './PartsMainContainer/ChatIntro';
 import style from './style.module.scss';
 
 function MainContainer() {
+  const [activeChat, setActiveChat] = useState({});
   return (
     <div className={style.contentarea}>
       <ChatIntro />
     </div>
-  )
+  );
 }
 
-export default MainContainer
+export default MainContainer;
