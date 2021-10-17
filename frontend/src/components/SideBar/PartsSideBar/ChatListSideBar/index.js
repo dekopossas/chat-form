644bf61ea0bbedcfd94b742ae7e393bef81ab199
deko possas <andrepossaster@gmail.com>
2021-10-17 00:04:24 -0300

    render itens chat list

---
+++ frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import ChatListItem from './ChatListComponents/ChatListItem';
+
+function ChatListSideBar() {
+  const [chatList, setChatList] = useState([{},{},{},{}]);  
+
+  return (
+    <div>
+      {chatList.map((contact, key)=>(
+        <ChatListItem
+          key={key}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ChatListSideBar
