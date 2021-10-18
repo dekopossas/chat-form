c33a9f3a611278159f7b653327299a20c48b81e8
deko possas <andrepossaster@gmail.com>
2021-10-18 00:22:38 -0300

    chat window

---
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatWindow() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatWindow
--- frontend/src/components/MainContainer/index.js
+++ frontend/src/components/MainContainer/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import ChatIntro from './PartsMainContainer/ChatIntro';
+import ChatWindow from './PartsMainContainer/ChatWindow';
 import style from './style.module.scss';
 
 function MainContainer() {
   const [activeChat, setActiveChat] = useState({});
+
   return (
     <div className={style.contentarea}>
-      <ChatIntro />
+      {
+        activeChat.id ? <ChatWindow /> : <ChatIntro />
+      }
     </div>
   );
 }
