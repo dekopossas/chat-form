8e40e28906db029e6c164ccb06abc5a42309d733
deko possas <andrepossaster@gmail.com>
2021-10-17 23:00:00 -0300

    render redux itens list

--- frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/index.js
+++ frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/index.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import ChatListItem from './ChatListComponents/ChatListItem';
 import style from './style.module.scss';
 import { connect } from 'react-redux';
@@ -6,17 +5,15 @@ import { connect } from 'react-redux';
 function ChatListSideBar(allReports) {
   return (
     <div className={style.chatList}>
-      {
-        allReports.allReports.map((contact, key) => (
-          <ChatListItem key={key} />
-        ))
-      }
+      {allReports.allReports.map((contact, key) => (
+        <ChatListItem key={key} />
+      ))}
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
-  allReports: state.chatReducer.allReports
-})
+  allReports: state.chatReducer.allReports,
+});
 
 export default connect(mapStateToProps)(ChatListSideBar);
