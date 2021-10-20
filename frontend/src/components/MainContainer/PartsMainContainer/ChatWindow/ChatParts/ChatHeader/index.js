03776c33a1acae1cbcfcdc2eb722ab7c6455d5f6
deko possas <andrepossaster@gmail.com>
2021-10-20 18:24:08 -0300

    icons that a cant use

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatHeader/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatHeader/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
+// Package
+import React from 'react';
 import style from './style.module.scss';
+
+// Ícons (mais uma vez icons q não consigo usar)
+// import SearchIcon from '@mui/icons-material/Search';
+// import AttachFileIcon from '@mui/icons-material/AttachFile';
+// import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function ChatHeader() {
   return (
     <div className={style.chatHeader}>
       <div className={style.chatInfo}>
         <img src="/image/avatares/avatar1.png" alt="" className={style.avatar} />
-        <div className={style.name}></div>
+        <div className={style.name}>André Possas</div>
+      </div>
+      <div className={style.header_buttons}>
+      <div className={style.btn}>
+          {/* <SearchIcon style={{color: '#919191'}}/> */}
+        </div>
+        <div className={style.btn}>
+          {/* <AttachFileIcon style={{color: '#919191'}}/> */}
+        </div>
+        <div className={style.btn}>
+          {/* <MoreHorizIcon style={{color: '#919191'}}/> */}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ChatHeader
+export default ChatHeader;
