61c7e67f0c307ad3c1eac17683b335e42c31c1a6
deko possas <andrepossaster@gmail.com>
2021-10-20 23:44:13 -0300

    some css styles emoji area

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import EmojiPicker from 'emoji-picker-react'
 
 import style from './style.module.scss';
+import './style.css';
 
 function EmojiArea() {
 
   const handleEmojiClick = () => {
-    
+
   }
 
   return (
     <div className={style.emoji}>
-      <EmojiArea disableSearchBar disableSkinPicker onEmojiClick={handleEmojiClick} />
+      <EmojiPicker disableSearchBar disableSkinPicker onEmojiClick={handleEmojiClick} />
     </div>
   )
 }
