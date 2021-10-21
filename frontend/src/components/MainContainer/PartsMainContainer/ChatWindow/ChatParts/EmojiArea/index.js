2aacc52e24bc427a2698c06e41a531072cf7a964
deko possas <andrepossaster@gmail.com>
2021-10-20 23:37:55 -0300

    some fixes

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
@@ -4,9 +4,14 @@ import EmojiPicker from 'emoji-picker-react'
 import style from './style.module.scss';
 
 function EmojiArea() {
+
+  const handleEmojiClick = () => {
+    
+  }
+
   return (
     <div className={style.emoji}>
-      <EmojiArea />
+      <EmojiArea disableSearchBar disableSkinPicker onEmojiClick={handleEmojiClick} />
     </div>
   )
 }
