c83a8e8d13648a943c134f2144a6d8dce78e3b10
deko possas <andrepossaster@gmail.com>
2021-10-20 23:53:34 -0300

    opem and close emoji begin

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EmojiPicker from 'emoji-picker-react'
 
 import style from './style.module.scss';
 import './style.css';
 
 function EmojiArea() {
+  const [emojiOpem, setEmojiOpem] = useState(false);
 
   const handleEmojiClick = () => {
 
   }
 
   return (
-    <div className={style.emoji}>
+    <div className={emojiOpem ? style.emoji : style.emojiOpem}>
       <EmojiPicker disableSearchBar disableSkinPicker onEmojiClick={handleEmojiClick} />
     </div>
   )
