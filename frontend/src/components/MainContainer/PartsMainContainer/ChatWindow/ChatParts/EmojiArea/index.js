4ece92f4cc9842e1688637eba949117aa35f3938
deko possas <andrepossaster@gmail.com>
2021-10-20 23:35:16 -0300

    emoji area

---
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/EmojiArea/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import EmojiPicker from 'emoji-picker-react'
+
+import style from './style.module.scss';
+
+function EmojiArea() {
+  return (
+    <div className={style.emoji}>
+      <EmojiArea />
+    </div>
+  )
+}
+
+export default EmojiArea
