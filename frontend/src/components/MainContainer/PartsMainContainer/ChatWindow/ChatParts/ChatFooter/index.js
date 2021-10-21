fa262011a6d958ddcdf86a81ad47e1dbed4da6cb
deko possas <andrepossaster@gmail.com>
2021-10-20 22:23:48 -0300

    style done

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatFooter/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatFooter/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import style from './style.module.scss';
 
 // import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
@@ -10,20 +10,16 @@ function ChatFooter() {
   return (
     <div className={style.chatFooter}>
       <div className={style.chatPre}>
-        <div className={style.btn}>
-          {/* <InsertEmoticonIcon style={{color: '#919191'}}/> */}
-        </div>
+        <div className={style.btn}>{/* <InsertEmoticonIcon style={{color: '#919191'}}/> */}</div>
       </div>
-      <div className={style.chatInput}>
-
+      <div className={style.chatInputArea}>
+        <input type="text" className={style.chatInput} placeholder="Digite uma mensagem" />
       </div>
       <div className={style.chatPos}>
-        <div className={style.btn}>
-          {/* <SendIcon style={{color: '#919191'}}/> */}
-        </div>
+        <div className={style.btn}>{/* <SendIcon style={{color: '#919191'}}/> */}</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ChatFooter
+export default ChatFooter;
