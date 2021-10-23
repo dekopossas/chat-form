857045bf8ddd2f1823604d7882ead7f6ca9c926d
deko possas <andrepossaster@gmail.com>
2021-10-23 15:32:04 -0300

    new login page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import style from './style.module.scss';
 import Form from '../../components/Form';
 
 function Home() {
@@ -8,8 +9,55 @@ function Home() {
   const goSuport = () => history.push('/suport');
 
   return (
-    <div>
-      <Form />
+    <div className={style.container}>
+      <div className={style.first_content}>
+        <div className={style.first_column}>
+          <h2 className={style.title}>Bem vindo de volta!</h2>
+          <p className={style.description}>Conecte-se agora</p>
+          <button className={style.btn}>Entrar</button>
+        </div>
+        <div className={style.second_column}>
+          <h2 className={style.title}>Criar Conta</h2>
+          <div className={style.social_link}>
+            <ul>
+              <li><a href="#">facebook</a></li>
+              <li><a href="#">google</a></li>
+              <li><a href="#">linkedin</a></li>
+            </ul>
+          </div>
+          <p className={style.description}>Ou preencha os campos a baixo</p>
+          <form className={style.form}>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit" className={style.btn} >Cadastrar</button>
+          </form>
+        </div>
+      </div>
+      <div className={style.second_content}>
+      <div className={style.first_column}>
+          <h2 className={style.title}>Bem vindo de volta!</h2>
+          <p className={style.description}>Conecte-se agora</p>
+          <button className={style.btn}>Entrar</button>
+        </div>
+        <div className={style.second_column}>
+          <h2 className={style.title}>Criar Conta</h2>
+          <div className={style.social_link}>
+            <ul>
+              <li><a href="#">facebook</a></li>
+              <li><a href="#">google</a></li>
+              <li><a href="#">linkedin</a></li>
+            </ul>
+          </div>
+          <p className={style.description}>Ou preencha os campos a baixo</p>
+          <form className={style.form}>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit" className={style.btn} >Cadastrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
