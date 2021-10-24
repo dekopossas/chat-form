cca5ef046fc1fb163a0f181365bcf93a8cd93360
deko possas <andrepossaster@gmail.com>
2021-10-24 14:16:03 -0300

    add suport key db

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Login from '../Login'
+import React from 'react';
+import Login from '../Login';
 
 function Home() {
   return (
     <div>
       <Login />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
