0bb8bd5c6d7a670af6cc8d6b496095f0e785ac49
deko possas <andrepossaster@gmail.com>
2021-10-24 14:29:40 -0300

    refactor directores

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Login from './pages/Login';
 import Suport from './pages/Suport';
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact component={Login} />
       <Route path="/suport" exact component={Suport} />
     </Switch>
   );
