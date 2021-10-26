5cd830beee1e3b79c4adef54dec9766f62e6b335
deko possas <andrepossaster@gmail.com>
2021-10-26 20:03:15 -0300

    new chat reduxer refect

--- frontend/src/redux/reducers/chatReducer.js
+++ frontend/src/redux/reducers/chatReducer.js
@@ -1,11 +1,5 @@
 const INITIAL_STATE = {
-  user: {
-    id: '',
-    avatar: '',
-    name: '',
-  },
   selectedPerson: {},
-  // dataBase: {},
 };
 
 const chatReducer = (state = INITIAL_STATE, action) => {
@@ -14,21 +8,6 @@ const chatReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         selectedPerson: action.person,
-      };
-    case 'SET_DATA':
-      return {
-        ...state,
-        dataBase: action.data,
-      };
-    case 'SET_USER':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          id: action.user.id,
-          avatar: action.user.avatar,
-          name: action.user.name,
-        },
       };
     default:
       return state;
