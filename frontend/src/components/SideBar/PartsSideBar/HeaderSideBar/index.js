6c392b65bfc7aa694e328860b5b56730d8a1fea4
deko possas <andrepossaster@gmail.com>
2021-11-03 19:55:55 -0300

    remove icons

--- frontend/src/components/SideBar/PartsSideBar/HeaderSideBar/index.js
+++ frontend/src/components/SideBar/PartsSideBar/HeaderSideBar/index.js
@@ -2,11 +2,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// Icons (não consegui usar os indos decidi não perder tempo aqui mais!)
-// import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-// import ChatIcon from '@mui/icons-material/Chat';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
-
 // Styles
 import style from './style.module.scss';
 
@@ -19,15 +14,12 @@ function HeaderSideBar(prop) {
       <div className={style.header_buttons}>
         <div className={style.header_icons}>
           <i class="fas fa-sync-alt" style={{ color: '#919191' }}></i>
-          {/* <DonutLargeIcon style={{color: '#919191'}} /> */}
         </div>
         <div className={style.header_icons}>
           <i class="fas fa-comments" style={{ color: '#919191' }}></i>
-          {/* <ChatIcon style={{color: '#919191'}} /> */}
         </div>
         <div className={style.header_icons}>
           <i class="fas fa-list-ul" style={{ color: '#919191' }}></i>
-          {/* <MoreVertIcon style={{color: '#919191'}} /> */}
         </div>
       </div>
     </div>
