1302b1540f0bb6c2c73168812c2385e125ff4aca
deko possas <andrepossaster@gmail.com>
2021-11-06 14:01:42 -0300

    redirect

--- frontend/src/pages/Suport/index.js
+++ frontend/src/pages/Suport/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 import SideBar from '../../components/SideBar';
 import MainContainer from '../../components/MainContainer';
+import { Redirect } from 'react-router';
 
 function Suport() {
+  const player = JSON.parse(localStorage.getItem('player'));
+
+  if(!player) return <Redirect to="/" />
+  
   return (
     <div className="app-window">
       <SideBar />
