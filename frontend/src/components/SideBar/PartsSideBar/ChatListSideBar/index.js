ce07a0ea625198ae6cb0bc697a9d7bc82feeac4e
deko possas <andrepossaster@gmail.com>
2021-11-07 22:48:02 -0300

    give up show the last msg hahaha

--- frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/index.js
+++ frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/index.js
@@ -14,12 +14,6 @@ function ChatListSideBar(prop) {
     setData(response.data);
   };
 
-  let lastMsg;
-
-  if (data) {
-    lastMsg = data[1].chat[data[1].chat.length - 1];
-  }
-
   useEffect(() => {
     loadData();
   }, []);
@@ -35,7 +29,6 @@ function ChatListSideBar(prop) {
               avatar={contact.avatar}
               active={contact.name === prop.person.name}
               onClick={() => prop.selectChat(contact)}
-              lastMsg={lastMsg.body}
             />
           ))
         ) : (
