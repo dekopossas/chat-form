9453a28b318b6694b70a8a9144235ea123d562bf
deko possas <andrepossaster@gmail.com>
2021-11-09 20:58:48 -0300

    i18n new suport

--- frontend/src/components/SideBar/PartsSideBar/NewSuport/index.js
+++ frontend/src/components/SideBar/PartsSideBar/NewSuport/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import style from './style.module.scss';
 
+import { useTranslation } from 'react-i18next';
+
+
 function NewSuport({ show, setShow }) {
   const handleClose = () => setShow(false);
+  const { t } = useTranslation();
+
 
   return (
     <div className={style.newChat} style={{ left: show ? 0 : -415 }}>
@@ -10,12 +15,12 @@ function NewSuport({ show, setShow }) {
         <div className={style.newChatBackButton} onClick={handleClose}>
           <i className="fas fa-arrow-left" style={{ color: '#FFF' }} />
         </div>
-        <div className={style.newChatHeadTitle}>Novo Atendimento</div>
+        <div className={style.newChatHeadTitle}>{t('New Suport')}</div>
       </div>
       <div className={style.newChatList}>
         <div className={style.newChatItem}>
           <img className={style.newChatItemAvatar} src="/image/avatares/plus.png" alt="newchat" />
-          <div className={style.newChatItemName}>Deseja Iniciar Um Novo Atendimento?</div>
+          <div className={style.newChatItemName}>{t('Do you want to start a new service?')}</div>
         </div>
       </div>
     </div>
