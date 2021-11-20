3252d61ba3f233ade8748cb4d7a4648a132e7433
deko possas <andrepossaster@gmail.com>
2021-11-19 23:27:24 -0300

    new chat format

--- frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatBody/MenssageItem/index.js
+++ frontend/src/components/MainContainer/PartsMainContainer/ChatWindow/ChatParts/ChatBody/MenssageItem/index.js
@@ -7,12 +7,12 @@ function MenssageItem({ data, player }) {
     <div
       className={style.mensageLine}
       style={{
-        justifyContent: player.id === data.author ? 'flex-end' : 'flex-start',
+        justifyContent: 'bot' === data.author ? 'flex-start' : 'flex-end',
       }}
     >
       <div
         className={style.mensageItem}
-        style={{ backgroundColor: player.id === data.author ? '#dcf8c6' : '#fff' }}
+        style={{ backgroundColor: 'bot' === data.author ? '#fff' : '#dcf8c6' }}
       >
         <div className={style.mensageText}>{data.body}</div>
         <div className={style.mensageDate}>19:00</div>
