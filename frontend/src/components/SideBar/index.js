0b5f762f196b2461c5faeb1428400857b90d9260
deko possas <andrepossaster@gmail.com>
2021-11-21 23:43:32 -0300

    api struturing

--- frontend/src/components/SideBar/index.js
+++ frontend/src/components/SideBar/index.js
@@ -10,8 +10,23 @@ import NewSuport from './PartsSideBar/NewSuport';
 // Styles
 import style from './style.module.scss';
 
+// API
+import api from '../../../../services/api';
+
+
 function SideBar() {
   const [newChat, setNewChat] = useState();
+  const model = {
+    name: '',
+    avatar: '',
+    chat: []
+  }
+
+  const fetchNewChat = async () => {
+    const response = await api.post('/suport', model);
+
+  };
+  
   const handleNewChat = () => {};
 
   const [showNewSuport, setShowNewSuport] = useState(false);
