2d40ed527abcbfc795c10d67d346d4a9cff4ea1b
Deko Possas <andrepossaster@gmail.com>
2021-12-09 22:24:46 -0300

    chat time saving

--- frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/ChatListComponents/ChatListItem.js
+++ frontend/src/components/SideBar/PartsSideBar/ChatListSideBar/ChatListComponents/ChatListItem.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import style from './style.module.scss';
-// import moment from 'moment';
 
-function ChatListItem({ onClick, active, data, avatar, lastMsg }) {
-  // const formateDate = (date) => {
-  //   return moment(date).format('DD/MM');
-  // };
+function ChatListItem({ onClick, active, data, avatar }) {
+  const lastMsgData = data.chat[data.chat.length - 1].time;
+  const formateDate = (date) => {
+    return `${date.slice(8, 10)}/${date.slice(5, 7)} ${date.slice(11, 16)}`;
+  };
 
   return (
     <div onClick={onClick} className={active ? style.chatListItemactive : style.chatListItem}>
-      <img
-        className={style.chatList_avatar}
-        src={avatar}
-        alt="screen user chat"
-      />
+      <img className={style.chatList_avatar} src={avatar} alt="screen user chat" />
       <div className={style.chatList_lines}>
         <div className={style.chatList_line}>
           <div className={style.chatList_nickname}>{data.name}</div>
-          <div className={style.chatList_date}>19:00</div>
+          <div className={style.chatList_date}>{formateDate(lastMsgData)}</div>
         </div>
         <div className={style.chatList_line}>
           <div className={style.chatList_lastMsg}>
