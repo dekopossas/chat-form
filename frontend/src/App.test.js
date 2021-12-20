387618dbc1231745c10cbfbc217cb6b40c092fab
deko possas <andrepossaster@gmail.com>
2021-12-20 20:42:31 -0300

    test file create

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Login from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('home test', () => {
+  it('element test', () => {
+    render(<Login />);
+    const linkElement = screen.getByText(/Forgot your password?/i);
+    expect(linkElement).toBeInTheDocument();
+  })
 });
