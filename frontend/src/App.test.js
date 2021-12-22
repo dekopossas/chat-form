f22680f04203082c6e95c6e113cfad9fd088950d
Deko Possas <andrepossaster@gmail.com>
2021-12-21 23:56:53 -0300

    new test of create section

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,10 +6,17 @@ describe('home test', () => {
     render(<Login />);
     const linkElement = screen.getByText(/Forgot your password?/i);
     expect(linkElement).toBeInTheDocument();
-  })
+  });
+
   it('login section', () => {
     render(<Login />);
     const linkElement = screen.getByText(/I already have an account!/i);
     expect(linkElement).toBeInTheDocument();
-  })
+  });
+
+  it('create section', () => {
+    render(<Login />);
+    const linkElement = screen.getByText(/Sign in with:/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
