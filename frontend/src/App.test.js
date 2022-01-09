f51041aca0c1ac89fe2c999f0d69ec37e0143c31
Andre Possas <andrepossaster@gmail.com>
2022-01-09 19:33:29 -0300

    new tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Login from './App';
+import Suport from './pages/Suport';
 
 describe('home test', () => {
   it('new password', () => {
@@ -16,6 +17,26 @@ describe('home test', () => {
 
   it('create section', () => {
     render(<Login />);
+    const linkElement = screen.getByText(/Sign in with:/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+describe('main screen test', () => {
+  it('new password', () => {
+    render(<Suport />);
+    const linkElement = screen.getByText(/Forgot your password?/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('Suport section', () => {
+    render(<Suport />);
+    const linkElement = screen.getByText(/I already have an account!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('create section', () => {
+    render(<Suport />);
     const linkElement = screen.getByText(/Sign in with:/i);
     expect(linkElement).toBeInTheDocument();
   });
